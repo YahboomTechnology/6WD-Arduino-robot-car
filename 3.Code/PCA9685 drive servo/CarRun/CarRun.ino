2881ce387bed283c321c64bf3525afcb50e84089
zzx <2448532184@qq.com>
2019-09-04 16:53:30 +0800

    First upload

---
+++ 3.Code/PCA9685 drive servo/CarRun/CarRun.ino
@@ -0,0 +1,77 @@
+/**
+* @par Copyright (C): 2010-2019, Shenzhen Yahboom Tech
+* @file         CarRun.c
+* @author       liusen
+* @version      V1.0
+* @date         2017.07.25
+* @brief        CarRun
+* @details
+* @par History  
+*
+*/
+#include <Wire.h>
+#include "Adafruit_PWMServoDriver.h"
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+
+
+/**
+* Function       setup
+* @author        liusen
+* @date          2017.07.25
+* @brief         initialization configure
+* @param[in]     void
+* @retval        void
+* @par History   
+*/
+void setup()
+{
+    Serial.begin(9600);
+    pwm.begin();
+  
+    pwm.setPWMFreq(50);  // Analog servos run at ~60 Hz updates
+}
+
+/**
+* Function       SetServoPos
+* @author        liusen
+* @date          2018.05.07
+* @brief         initialization configure
+* @param[in]     num: number of servo； value：angle of servo：0-180
+* @retval        void
+* @par History   
+*/
+void SetServoPos(int num, int value)
+{
+    // 50hz: 20,000 us
+    long us = (value * 1800 / 180 + 600); // 0.6 ~ 2.4
+    long pwmvalue = us * 4096 / 20000;
+    pwm.setPWM(num, 0, pwmvalue);
+}
+
+/**
+* Function       loop
+* @author        liusen
+* @date          2017.07.25
+* @brief        
+*                
+* @param[in]     void
+* @retval        void
+* @par History   
+*/
+
+void loop()
+{
+
+    SetServoPos(0, 90);//K9
+    SetServoPos(1, 90);//K10
+    SetServoPos(2, 90);//K11
+    SetServoPos(3, 90);//K12
+    SetServoPos(4, 90);//K13
+    SetServoPos(5, 90);//K14
+    SetServoPos(6, 90);//K15
+    SetServoPos(7, 90);//K16
+
+}
